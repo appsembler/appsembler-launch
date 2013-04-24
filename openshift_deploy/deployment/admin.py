901a8f1e0d3de739df511ba3f9c2568e8ed58697
Filip Jukic <filip@jukic.me>
2013-04-24 16:57:37 +0200

    added the admin site

---
+++ openshift_deploy/deployment/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Deployment, Project
+
+admin.site.register(Deployment)
+admin.site.register(Project)
--- openshift_deploy/openshift_deploy/urls.py
+++ openshift_deploy/openshift_deploy/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import include, patterns, url
+from django.contrib import admin
 from deployment.views import MainView
 from .api import v1_api
 
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^api/', include(v1_api.urls)),
-    url(r'^$', MainView.as_view(), name='main'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', MainView.as_view(), name='main'),  
 )
