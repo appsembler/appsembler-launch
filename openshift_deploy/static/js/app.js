b898d2b30ec74f52c2d33e73673b0f74998f40dc
Filip Jukic <filip@jukic.me>
2013-05-01 14:55:14 +0200

    separate template for deployment status added

--- openshift_deploy/static/js/app.js
+++ openshift_deploy/static/js/app.js
@@ -41,19 +41,22 @@ var AppView = Backbone.View.extend({
     },
 
     deploy: function(e) {
-        $("#info-message").show();
-        this.$("button[type=submit]").hide();
+        e.preventDefault();
+        this.showInfoWindow();
         var project_uri = this.$('select[name=project]').val();
         var email = this.$('input[name=email]').val();
         var deploy = new Deployment({
             project: project_uri,
             email: email
         });
-        deploy.save({}, {
+        /*deploy.save({}, {
             success: this.deploymentSuccess,
             error: this.deploymentFail
-        });
-        e.preventDefault();
+        });*/
+    },
+
+    showInfoWindow: function() {
+        this.$el.html($("#deploy_status_template").html());
     },
 
     deploymentSuccess: function(model, response, options) {
