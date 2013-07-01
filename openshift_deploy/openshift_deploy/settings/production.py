254c339327fd7bb42c4f1940aa2159fbb60225ee
Filip Jukic <filip@jukic.me>
2013-07-01 14:42:42 -0400

    changed production database to postgresql

--- openshift_deploy/openshift_deploy/settings/production.py
+++ openshift_deploy/openshift_deploy/settings/production.py
@@ -6,13 +6,13 @@ STATIC_ROOT = os.path.join(get_env_variable('OPENSHIFT_REPO_DIR'), 'wsgi', 'stat
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(get_env_variable('OPENSHIFT_DATA_DIR'), 'sqlite3.db'),  # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'appsemblerlaunch',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': get_env_variable('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+        'PASSWORD': get_env_variable('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+        'HOST': get_env_variable('OPENSHIFT_POSTGRESQL_DB_HOST'),                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': get_env_variable('OPENSHIFT_POSTGRESQL_DB_PORT'),                      # Set to empty string for default.
     }
 }
 
