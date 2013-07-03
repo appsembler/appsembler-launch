139d82fc51ea9cdddb35684d8018ffce06e58c79
Filip Jukic <filip@jukic.me>
2013-07-03 09:22:16 -0400

    switched local db to postgres (use postgres.app on os x)

--- openshift_deploy/openshift_deploy/settings/local.py
+++ openshift_deploy/openshift_deploy/settings/local.py
@@ -10,6 +10,18 @@ EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'appsemblerlaunch',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 
 INTERNAL_IPS = (
     '127.0.0.1',
@@ -33,6 +45,6 @@ CELERYBEAT_SCHEDULE = {
     # },
     'destroy-expired-apps': {
         'task': 'deployment.tasks.destroy_expired_apps',
-        'schedule': timedelta(seconds=10),
+        'schedule': timedelta(seconds=1000),
     },
 }
